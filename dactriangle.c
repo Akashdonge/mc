731bc9b5c1f3086f7f8675174df41f6703be2065
Akash donge <114501810+Akashdonge@users.noreply.github.com>
2024-08-08 21:55:41 +0530

    Create dactriangle.c

---
+++ dactriangle.c
@@ -0,0 +1,40 @@
+#include <msp430x44x.h> 
+#include <in430.h> 
+#include “initdevice.h” 
+ 
+void port_init(void 
+ 
+unsigned int i; 
+ char count; 
+ 
+void port_init(void) 
+{ 
+ 
+  P2SEL |= 0x00; 
+  P2DIR |= 0xFF; 
+  P2OUT |= 0x00; 
+ 
+} 
+ 
+ 
+void main(void) 
+{ 
+  WDTCTL = WDTPW+WDTHOLD;         // Stop watchdog timer 
+     init_devices(); 
+port_init(); 
+  for ( i=0; i<0x3600; i++);           // Delay for reference start-up 
+ 
+  count = 0; 
+ while(1) 
+ { 
+  for(count=0;count<=0x3F;count++) 
+  { 
+   P2OUT = count ; 
+  } 
+  for(count=0x3F;count>0;count--) 
+  { 
+   P2OUT = count ; 
+  } 
+  
+ } 
+}
